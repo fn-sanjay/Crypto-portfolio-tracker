cbdf872c23f12d19bfd0421b773b6fc6f9631824
Ingrimmer <ingrimmer59@gmail.com>
2024-10-10 17:02:56 +0530

    second commit

--- controllers/pricesController.mjs
+++ controllers/pricesController.mjs
@@ -7,7 +7,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-dotenv.config({ path: path.resolve(__dirname, '../../.env') });
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 const url = 'https://api.coingecko.com/api/v3/coins/markets';
 const options = {
